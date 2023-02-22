dc1d538da6f2c43c46ee963764feb58552e65f0e
Nurullah Nergiz <59697823+Nurullah-Nergiz@users.noreply.github.com>
2023-02-23 00:20:06 +0300

    Update footer

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,5 +1,5 @@
 <template>
     <footer>
-        Bu web sitesinin kaynak kodlarına <a href="https://github.com/Nurullah-Nergiz/"><b>Github üzerinden</b></a> ulaşabilirsiniz.
+        Bu web sitesinin kaynak kodlarına <a href="https://github.com/Nurullah-Nergiz/webpage"><b>Github üzerinden</b></a> ulaşabilirsiniz.
     </footer>
 </template>
