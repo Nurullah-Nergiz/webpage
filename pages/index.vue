f33c87ced3ca7e92998cdf8d5c8985b7ef8a4056
Nurullah Nergiz <59697823+Nurullah-Nergiz@users.noreply.github.com>
2023-04-08 00:56:05 +0300

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -42,9 +42,6 @@ export default {
   <section class="contact">
     <h1>İletişim</h1>
     <span>
-      <a href="tel:+905457241456" class="bx bx-phone">
-        <p>Telefon</p>
-      </a>
       <a href="mailto:nurullah-nergiz@hotmail.com" class="bx bx-envelope"
         ><p>Email</p>
       </a>
