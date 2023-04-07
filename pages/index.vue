ad6c5f14f47cb895c908863e4c9eb47287a1ce06
Nurullah Nergiz <59697823+Nurullah-Nergiz@users.noreply.github.com>
2023-04-08 00:49:51 +0300

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -39,6 +39,29 @@ export default {
       e-posta veya LinkedIn aracılığıyla ulaşabilirsiniz.
     </div>
   </section>
+  <section class="contact">
+    <h1>İletişim</h1>
+    <span>
+      <a href="tel:+905457241456" class="bx bx-phone">
+        <p>Telefon</p>
+      </a>
+      <a href="mailto:nurullah-nergiz@hotmail.com" class="bx bx-envelope"
+        ><p>Email</p>
+      </a>
+      <a
+        href="https://github.com/Nurullah-Nergiz/"
+        target="_blank"
+        class="bx bxl-github"
+        ><p>Github</p>
+      </a>
+      <a
+        href="https://www.linkedin.com/in/nurullah-nergiz-375674221/"
+        target="_blank"
+        class="bx bxl-linkedin"
+        ><p>Linkedin</p>
+      </a>
+    </span>
+  </section>
 </template>
 
 <style></style>
