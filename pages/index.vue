a4f2377de945ad21e48f9ac0f1466fc9be1b360c
Nurullah Nergiz <59697823+Nurullah-Nergiz@users.noreply.github.com>
2023-04-16 12:34:28 +0300

    About update

--- pages/index.vue
+++ pages/index.vue
@@ -20,23 +20,27 @@ export default {
 
 <template>
   <section class="about">
-    <h2>
+    <h1>
       <b>Merhaba Ben Nurullah</b><br />
-      Konya'da yaşayan bir yazılımcıyım
-    </h2>
+    </h1>
     <br />
-    <div class="descriptions">
-      Ben bir web geliştiricisiyim. Bu portföyüm, web tasarım ve geliştirme
-      projelerimdeki yeteneklerimi ve deneyimimi sergiliyor. Front-end
-      teknolojileri, HTML, CSS ve JavaScript'te derin bir bilgiye sahibim ve
-      React Vue gibi modern JavaScript çerçevelerinde deneyimliyim. Back-end
-      olarak Node.js, Express ve MongoDB gibi teknolojilerde de deneyimim
-      bulunmaktadır. Bu projeler, müşterilerime veya kişisel ilgi alanlarıma
-      yönelik işleri içerir. Yaratıcı fikirlerimle ve problem çözme yeteneğimle
-      birlikte güzel, işlevsel ve kullanıcı dostu web siteleri geliştiriyorum.
-      Siz de, projelerim hakkında daha fazla bilgi edinmek için sayfaları
-      ziyaret edebilirsiniz. İletişime geçmek veya işbirliği yapmak için bana
-      e-posta veya LinkedIn aracılığıyla ulaşabilirsiniz. <br /><br />
+    <div class="descriptions">o
+Bir web yazılımcısıyım. Yaklaşık 5 yıldır bu alanda çalışıyorum ve bu süre boyunca birçok farklı proje için çalışma fırsatım oldu.
+
+Web geliştirme işine nasıl girdim? Aslında, ben her zaman bilgisayar teknolojilerine ilgi duydum ve bu alanda kendimi geliştirmek istedim. 
+Birçok farklı web geliştirme şirketinde çalıştım. Bu deneyimler, şu anda sahip olduğumuz bilgi ve becerilerin çoğunu geliştirmeme yardımcı oldu.
+
+Web geliştirme alanındaki en büyük tutkularımdan biri, web sitelerinin kullanıcı deneyimini geliştirmektir. Kullanıcılar için daha iyi bir deneyim sunmak için, her zaman araştırmalar yapıyor ve en son teknolojileri takip ediyorum. Kullanıcı dostu web siteleri tasarlama konusundaki uzmanlığım, müşterilerimin hedef kitlelerine erişmelerine yardımcı olurken, onların marka bilinirliğini de artırıyor.
+
+Benim için web geliştirme sadece bir iş değil, bir tutku. Kendimi sürekli olarak geliştiriyor ve daha iyi olmak için çaba sarf ediyorum. Müşterilerimle işbirliği yaparken, onların ihtiyaçlarını anlayarak ve onlara özel çözümler sunarak işlerini benzersiz bir şekilde tasarlıyorum.
+
+Benimle çalışmanın avantajlarından biri, müşterilerimin her zaman projelerinin ilerlemesini takip edebilecek olmasıdır. Ayrıca, projelerin her aşamasında müşterilerimle açık bir iletişim kurarım, böylece her zaman aynı sayfada olduğumuzdan emin olurum.
+
+Web geliştirme alanındaki uzmanlığımla, size özel bir web sitesi tasarlamak ve geliştirmek için sabırsızlanıyorum. Eğer benimle çalışmak isterseniz, bana \[email protected] adresinden ulaşabilirsiniz.
+
+Teşekkür ederim!
+
+ <br /><br />
     </div>
   </section>
   <section class="contact">
