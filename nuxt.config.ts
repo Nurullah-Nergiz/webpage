406ccb6a3d11b2b518ad47e79450ab3d763e4337
Nurullah Nergiz <59697823+Nurullah-Nergiz@users.noreply.github.com>
2023-08-11 13:15:02 +0300

    Update meta keywords

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -51,7 +51,7 @@ export default defineNuxtConfig({
         },
         {
           name: "keywords",
-          content: `Nurullah Nergiz,nurullah, nergiz,nurullah nergiz, Frontend developer`,
+          content: `Nurullah Nergiz,nurullah, nergiz,nurullah nergiz, Frontend developer, github, linkedin, `,
         },
         {
           name: "og:title",
