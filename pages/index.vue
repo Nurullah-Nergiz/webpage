73229bb02518233e139c08ad48d7c1f2eb941dbf
Nurullah Nergiz <59697823+Nurullah-Nergiz@users.noreply.github.com>
2023-08-23 22:01:23 +0300

    Update cv link

--- pages/index.vue
+++ pages/index.vue
@@ -31,13 +31,14 @@ export default {
         Since 2016, I have worked with web technologies.
       </p>
 
-      <a href="#"
-        class="mt-3 py-2 px-3 text-gray-500 bg-secondary border border-current rounded-br-lg rounded-tl-lg shadow-inner shadow-current inline-block">CV'yi
-        indir</a>
+      <a href="./cv.html" download
+        class="mt-3 py-2 px-3 text-gray-500 bg-secondary border border-current rounded-br-lg rounded-tl-lg shadow-inner shadow-current inline-block">
+        Download CV
+      </a>
       <br><br>
     </div>
     <div class="flex-1 md:max-w-xs md:px-5 flex flex-col gap-5">
-      <h2 class="text-2xl">Sosyal medya</h2>
+      <h2 class="text-2xl">Social Media</h2>
       <a href="https://github.com/Nurullah-Nergiz/" target="_blank"
         class="px-3 py-2 flex items-center gap-2 border border-current rounded-full ">
         <i class="bx bxl-github text-xl"></i>
