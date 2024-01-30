9a2d4f3584276b4c3ce58e72747effdeafd3e9e5
Nurullah-Nergiz <developer@nurullah.aleeas.com>
2024-01-30 10:19:23 +0300

    Update Footer

---
+++ components/Logo.vue
@@ -0,0 +1,12 @@
+<script>
+import logo_white from "@/assets/img/logo-white.png";
+export default {
+    data() {
+        return {
+        }
+    }
+}
+</script>
+<template>
+    <img src="../assets/img/logo-white.svg" alt="logo">
+</template>
